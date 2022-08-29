1ad7095861e540678ef74c1415a278920afb9624
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-08-29 11:12:11 -0300

    fix: add type annotations

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,17 @@
 module Main exposing (..)
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (button, div, text)
 import Html.Events exposing (onClick)
 import Dict exposing (update)
 
+type Msg = Increment | Decrement
+
+main : Program () Int Msg
 main =
     Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
 
+update : Msg -> number -> number
 update msg model =
   case msg of
     Increment ->
@@ -17,6 +20,7 @@ update msg model =
     Decrement ->
       model - 1
 
+view : Int -> Html.Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
